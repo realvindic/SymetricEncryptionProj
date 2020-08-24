2c044067ef236305f64765e70d257f981d3a909b
vindic <VINDIC@protonmail.com>
2020-08-24 19:57:30 -0400

    stoofs

--- main.cpp
+++ main.cpp
@@ -6,8 +6,6 @@ int encrypt(char *msg,int len){
     int i;
     for(i=0;i<len;i++){
         printf("%lf - %d\n",crypto(msg[i]),msg[i]);
-        double dmsg = decrypto(crypto(msg[i]));
-        printf("%lf \n",dmsg);
     }
 }
 int decrypt(char *emsg,int len){
@@ -17,11 +15,22 @@ int decrypt(char *emsg,int len){
     }
 }
 int main(){
-    char text[20];
-    scanf("%s",text);
-    int len = strlen(text);
-    BYTE arr[len];
+    printf("ecnrypt or decrypt");
+    if(getchar() == "encrypt"){
+        char text[20];
+        scanf("%s",text);
+        int len = strlen(text);
+        BYTE arr[len];
+        int i;
+        string2ByteArray(text,arr);
+        encrypt(text,len);
+        //gitKrakin test//
+}else{
+    int len;
+    scanf(" enter in how many words:%d",len);
     int i;
-    string2ByteArray(text,arr);
-    encrypt(text,len);
+    for(i=0;i<len;i++){
+        decrypto(getchar());
+    }
+}
 }
